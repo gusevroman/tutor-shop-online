71c30fc5b4873869c90cb00ea8622f1c65770c1d
gusevroman <949397@mail.ru>
2020-03-28 16:09:15 +0300

    error  @handlebars/allow-prototype-acces

--- models/course.js
+++ models/course.js
@@ -1,100 +1,16 @@
-const uuid = require('uuid');
-const fs = require('fs');
-const path = require('path');
-
-const uuidv4 = uuid.v4;
-const coursesDataPath = path.join(__dirname, '..', 'data', 'courses.json');
-
-class Course {
-  constructor(title, price, img) {
-    this.title = title;
-    this.price = price;
-    this.img = img;
-    this.id = uuidv4();
-  }
+const { Schema, model } = require('mongoose');
 
 
-  toJSON() {
-    return {
-      title: this.title,
-      price: this.price,
-      img: this.img,
-      id: this.id,
-    };
-  }
+const course = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  img: String,
+});
 
-  static async update(course) {
-    const courses = await Course.getAll();
-
-    const idx = courses.findIndex((el) => el.id === course.id);
-    courses[idx] = course;
-
-    fs.writeFile(
-      coursesDataPath,
-      JSON.stringify(courses),
-      (err) => {
-        if (err) throw err;
-      },
-    );
-
-
-    // return new Promise((resolve, reject) => {
-    //   fs.writeFile(
-    //     coursesDataPath,
-    //     JSON.stringify(courses),
-    //     (err) => {
-    //       if (err) {
-    //         reject(err);
-    //       } else {
-    //         resolve();
-    //       }
-    //     },
-    //   );
-    // });
-  }
-
-  async save() {
-    const courses = await Course.getAll();
-    courses.push(this.toJSON());
-
-    return new Promise((resolve, reject) => {
-      fs.writeFile(
-        coursesDataPath,
-        JSON.stringify(courses),
-        'utf-8',
-        (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve();
-          }
-        },
-
-      );
-    });
-  }
-
-  static getAll() {
-    return new Promise((resolve, reject) => {
-      fs.readFile(
-        coursesDataPath,
-        'utf-8',
-        (err, content) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(JSON.parse(content));
-          }
-        },
-      );
-    });
-  }
-
-  static async getById(id) {
-    const courses = await Course.getAll();
-    return courses.find((el) => el.id === id);
-  }
-}
-
-
-module.exports = Course;
+module.exports = model('Course', course);
--- routes/courses.js
+++ routes/courses.js
@@ -4,7 +4,8 @@ const Course = require('../models/course');
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const courses = await Course.getAll();
+  const courses = await Course.find({ price: 288 }).lean(); // added lean() for getting JSON instead of mongoose documents
+  console.log(courses);
   res.render('courses', {
     title: 'Courses',
     isCourses: true,
@@ -17,7 +18,7 @@ router.get('/:id/edit', async (req, res) => {
     return res.redirect('/');
   }
   const { id } = req.params;
-  const course = await Course.getById(id);
+  const course = await Course.findById({ id });
 
   res.render('course-edit', {
     title: `Edit ${course.title}`,
@@ -31,7 +32,7 @@ router.post('/edit', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  const course = await Course.getById(req.params.id);
+  const course = await Course.findById(req.params.id);
   res.render('course', {
     layout: 'empty',
     title: `Course ${course.title}`,
